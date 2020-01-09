0d26267d1a4eec9a48ee96b7782a8be0ad06c033
Serzas <j_sergej@yahoo.com>
2020-01-09 19:42:45 +0200

    cleaning code

--- numbers-generator/src/main/java/com/serjer/service/GeneratorService.java
+++ numbers-generator/src/main/java/com/serjer/service/GeneratorService.java
@@ -6,30 +6,24 @@ import com.serjer.model.Generator;
 
 @Service
 public class GeneratorService {
-
-	public long generateNextValue(long number, long factor, long divider) {
-		return (number * factor % divider);
-	}
-	
-	
-	public boolean isOverlap(long num1, long num2, int lastBitsNumber) {
-		int criteriaToCompare = Integer.parseInt("1".repeat(lastBitsNumber), 2);
-			
-		return ((num1 & criteriaToCompare) == (num2 & criteriaToCompare));
-	}
-	
+		
 	public long calculateMatches(long num1, long num2, Generator generator) {
 		int matches = 0;
-		
 		for (int i = 0; i < generator.getIterationNumber(); i++) {
-			
 			num1 = generateNextValue(num1, generator.getFactorA(), generator.getDivider());
 			num2 = generateNextValue(num2, generator.getFactorB(), generator.getDivider());
-				
 			if(isOverlap(num1, num2, generator.getLastBitsNumber())) 
 				matches++;
 		}
 		return matches;
 	}	
 	
+	public long generateNextValue(long number, long factor, long divider) {
+		return (number * factor % divider);
+	}
+		
+	public boolean isOverlap(long num1, long num2, int lastBitsNumber) {
+		int criteriaToCompare = Integer.parseInt("1".repeat(lastBitsNumber), 2);
+		return ((num1 & criteriaToCompare) == (num2 & criteriaToCompare));
+	}
 }
